f5fdcf6158c2ca0851b90bf6d13cafc107e6051c
Iamneo <techadmin@iamneo.ai>
2023-09-25 17:55:42 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroserviceone/Migrations/20230925175528_FlightInitialSetup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetmicroserviceone.Migrations
+{
+    /// <inheritdoc />
+    public partial class FlightInitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    FlightID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FlightNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TicketPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalSeats = table.Column<int>(type: "int", nullable: false),
+                    Airline = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pilot = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Terminal = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.FlightID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+        }
+    }
+}
